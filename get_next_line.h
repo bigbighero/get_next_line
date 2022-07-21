2ca4ffabbd0f0c649bf69f0a37e0d886677b3f8d
roferrei <tecnicorodrigo@outlook.com>
2022-07-22 00:50:33 +0200

    update some files

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 00:44:59 by roferrei          #+#    #+#             */
-/*   Updated: 2022/07/22 00:45:00 by roferrei         ###   ########.fr       */
+/*   Updated: 2022/07/22 00:49:17 by roferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
