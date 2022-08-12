7a19f0d350b51bc1e574794fefcfddb1e9953408
roferrei <tecnicorodrigo@outlook.com>
2022-08-12 02:51:20 +0200

    update some files

---
+++ get_next_line_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 01:00:04 by roferrei          #+#    #+#             */
+/*   Updated: 2022/08/12 02:19:01 by roferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start + 1)
+		return (ft_strdup(""));
+	if (ft_strlen(&s[start]) < len)
+		len = ft_strlen(&s[start]);
+	str = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
+}
+
+static char	*read_txt(char *rest, int fd)
+{
+	char	*buffer;
+	ssize_t	text_read;
+	char	*aux;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (ft_strchr(rest, '\n') == NULL)
+	{
+		text_read = read(fd, buffer, BUFFER_SIZE);
+		if (text_read <= 0)
+			break ;
+		buffer[text_read] = '\0';
+		aux = rest;
+		rest = ft_strjoin(rest, buffer);
+		free (aux);
+	}
+	free (buffer);
+	return (rest);
+}
+
+static void	save_me(char **rest)
+{
+	if (**rest == '\0')
+	{
+		free(*rest);
+		*rest = NULL;
+	}
+}
+
+static char	*get_line(char **rest)
+{
+	char	*i;
+	char	*temp;
+	char	*aux;
+
+	i = ft_strchr(*rest, '\n');
+	save_me(rest);
+	if (i == NULL)
+	{
+		temp = *rest;
+		*rest = NULL;
+	}
+	else
+	{
+		aux = *rest;
+		temp = ft_substr(*rest, 0, (i - *rest + 1));
+		if (i[1] != '\0')
+			*rest = ft_strdup(&i[1]);
+		else
+			*rest = NULL;
+		free(aux);
+	}
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*rest[1024];
+	char		*line;
+
+	if (rest[fd] == NULL)
+		rest[fd] = ft_strdup("");
+	rest[fd] = read_txt(rest[fd], fd);
+	line = get_line(&rest[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 00:44:59 by roferrei          #+#    #+#             */
+/*   Updated: 2022/08/12 02:20:40 by roferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
